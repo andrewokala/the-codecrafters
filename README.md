9025913399e4076db1df60d14f6aba2e0a6a1783
ahndrhuDev <ahndrhu25@gmail.com>
2026-03-27 08:23:35 +0100

    Calculator Code

---
+++ README.md
@@ -0,0 +1,10 @@
+# HOW TO RUN THE CALCULATOR
+
+## Step 1:
+In your CLI e.g Bash or Command Prompt, type go run calculator.go
+
+## Step 2:
+You'll be prompted to input your First Number for Calcultion and also your Second Number for calculation.
+
+## Step 3:
+After the program recieves your inputed numbers, it will output the result in the CLI. You can perform perform multiple calculations as long as you don't opt for
---
+++ thecodecrafterthon-day1/calculator.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+	func add(input1, input2 int) int {
+	return input1 + input2
+	}
+	func sub(input1, input2 int) int {
+	return input1 - input2
+	}
+
+	func mul(input1, input2 int) int {
+	return input1 * input2
+	}
+
+	func div(input1, input2 int) int {
+	return input1 / input2
+	}
+
+func main() {
+	for	{
+		var operation int
+		fmt.Println("Welcome For Calculation")
+		fmt.Println("1. Addition")
+		fmt.Println("2. Subtraction")
+		fmt.Println("3. Multiplication")
+		fmt.Println("4. Division")
+		fmt.Println("5. Exit")
+		fmt.Scan(&operation)
+
+		if operation == 5 {
+			fmt.Println("Goodbye ROYALTY")
+			break
+		}
+
+		if operation != operation  {
+			fmt.Println("WE ONLY DO CALCULATIONS!!!!!")
+			continue
+		}
+
+		var input1, input2 int
+		fmt.Println("Enter Input 1: ")
+		fmt.Scan(&input1)
+		fmt.Println("Enter Input 2: ")
+		fmt.Scan(&input2)
+
+		switch operation {
+		case 1:
+			fmt.Println(add(input1, input2))
+		case 2:
+			fmt.Println(sub(input1, input2))
+		case 3:
+			fmt.Println(mul(input1, input2))
+		case 4:
+			fmt.Println(div(input1, input2))
+		}
+	}
+
+
+}
