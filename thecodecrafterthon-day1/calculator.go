6be6d062d91da3e66e3176570553ce65817f2dc3
andrewokala <ahndrhu25@gmail.com>
2026-03-27 10:50:24 +0100

    Refined Calculator

--- thecodecrafterthon-day1/calculator.go
+++ thecodecrafterthon-day1/calculator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 
@@ -22,40 +23,58 @@ import (
 
 func main() {
 	for	{
-		var operation int
+		var operation string
+
+		var num1, num2 string
+		fmt.Println("Enter Input 1: ")
+		fmt.Scan(&num1)
+		input1, err1 := strconv.Atoi(num1)
+		if err1 != nil {
+			fmt.Println("Must Be A Number")
+			continue
+			}
+		fmt.Println("Enter Input 2: ")
+		fmt.Scan(&num2)
+		input2, err2 := strconv.Atoi(num2)
+		if err2 != nil {
+			fmt.Println("Must Be A Number")
+			continue
+		}
+
 		fmt.Println("Welcome For Calculation")
 		fmt.Println("1. Addition")
 		fmt.Println("2. Subtraction")
 		fmt.Println("3. Multiplication")
 		fmt.Println("4. Division")
 		fmt.Println("5. Exit")
+		fmt.Println("Help for Use Cases")
 		fmt.Scan(&operation)
 
-		if operation > 5 {
-			fmt.Println("Invalid Input!!!!!")
-			break
-		}
-
-		if operation == 5 {
-			fmt.Println("Goodbye ROYALTY")
-			break
-		}
-
-				var input1, input2 int
-		fmt.Println("Enter Input 1: ")
-		fmt.Scan(&input1)
-		fmt.Println("Enter Input 2: ")
-		fmt.Scan(&input2)
-
 		switch operation {
-		case 1:
+		case "1":
+			
 			fmt.Println(add(input1, input2))
-		case 2:
+		case "2":
 			fmt.Println(sub(input1, input2))
-		case 3:
+		case "3":
 			fmt.Println(mul(input1, input2))
-		case 4:
+		case "4":
+			if input2 == 0 {
+				fmt.Println("Not A Divisor")
+				continue
+			}
 			fmt.Println(div(input1, input2))
+		case "5":
+			fmt.Println("Goodbye ROYALTY")
+			return
+		case "help":
+			fmt.Println("USE CASE")
+			fmt.Println("1 For Addirion")
+			fmt.Println("2 For Subtraction")
+			fmt.Println("3 For Multiplication")
+			fmt.Println("4 For Addition")
+			fmt.Println("5 To Exit")
+		
 		default:
 			fmt.Println("Not Supported")
 			continue
